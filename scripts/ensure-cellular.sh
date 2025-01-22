05b4c27dd4a7e7b22c2789727b5145823fd23186
Garrett Mesmer <190508517+gmesm@users.noreply.github.com>
2025-01-22 16:25:00 -0500

    add chroot method for creating NetworkManager profile

--- scripts/ensure-cellular.sh
+++ scripts/ensure-cellular.sh
@@ -19,14 +19,39 @@ while getopts "${opstr}" OPTION; do
 done
 
 if [ ! -z "$APN" ]; then
-  echo "Removing existing network manager profile for Cellular..."
-  $SUDO nmcli con delete 'Cellular'
-  echo "Adding network manager profile for Cellular..."
-  $SUDO nmcli connection add type gsm ifname cdc-wdm0 con-name "Cellular" apn "$APN" connection.autoconnect yes
-  echo "Waiting for conneciton to come up..."
-  sleep 5
-  $SUDO nmcli con show
-  true
+  if ischroot ; then
+    echo "Removing existing network manager profile for Cellular..."
+    $SUDO rm -f /etc/NetworkManager/system-connections/Cellular*.nmconnection
+    echo "Adding network manager profile for Cellular..."
+    echo "[connection]
+id=Cellular
+uuid=$(uuidgen)
+type=gsm
+interface-name=cdc-wdm0
+
+[gsm]
+apn=$APN
+
+[ipv4]
+method=auto
+
+[ipv6]
+addr-gen-mode=stable-privacy
+method=auto
+
+[proxy]
+" | $SUDO tee /etc/NetworkManager/system-connections/Cellular.nmconnection
+    $SUDO chmod 600 /etc/NetworkManager/system-connections/Cellular.nmconnection
+  else
+    echo "Removing existing network manager profile for Cellular..."
+    $SUDO nmcli con delete 'Cellular'
+    echo "Adding network manager profile for Cellular..."
+    $SUDO nmcli connection add type gsm ifname cdc-wdm0 con-name "Cellular" apn "$APN" connection.autoconnect yes
+    echo "Waiting for conneciton to come up..."
+    sleep 5
+    $SUDO nmcli con show
+    true
+  fi
 else
   echo "APN cannot be blank, doing nothing!"
   false
